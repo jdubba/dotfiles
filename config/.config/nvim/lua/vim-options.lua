fa8de459f866a175a0654dca4ad6d7fac46f83dd
jdubba <148065+jdubba@users.noreply.github.com>
2025-05-08 10:26:49 -0500

    Import and cleanup of basic set of config files for alacritty, kitty, git, neovim, and starship

---
+++ config/.config/nvim/lua/vim-options.lua
@@ -0,0 +1,32 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "120"
+
+vim.cmd("highlight Normal guibg=none")
+vim.cmd("highlight NonText guibg=none")
+vim.cmd("highlight Normal ctermbg=none")
+vim.cmd("highlight Nontext ctermbg=none")
+vim.g.mapleader = " "
+
+
---
+++ config/.config/nvim/lua/vim-remaps.lua
@@ -0,0 +1,38 @@
+vim.keymap.set("n", "<leader>ft", vim.cmd.Ex, {})
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+--Quick copy to system keyboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+--Split Navigation
+vim.keymap.set("n", "<C-j>", ":wincmd j<cr>")
+vim.keymap.set("n", "<C-k>", ":wincmd k<cr>")
+vim.keymap.set("n", "<C-h>", ":wincmd h<cr>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<cr>")
+
+--Search
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left>")
+
+--Buffer Navigation
+vim.keymap.set("n", "<leader>n", ":bn<cr>")
+vim.keymap.set("n", "<leader>p", ":bp<cr>")
+vim.keymap.set("n", "<leader>x", ":bd<cr>")
+
+--Comment Toggle
+vim.keymap.set({"n", "v"}, "<leader>/", ":CommentToggle<cr>")
+
+--Indentation
+vim.keymap.set({"n", "v"}, "<leader>>", ":><cr>")
+vim.keymap.set({"n", "v"}, "<leader><", ":<<cr>")
