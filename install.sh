e854ec12a12e3e30e6b97296ae24ce0db8f5ccd8
jdubba <148065+jdubba@users.noreply.github.com>
2025-05-08 12:17:41 -0500

    Added basic package installation and key directory creation.

--- install.sh
+++ install.sh
@@ -3,4 +3,6 @@
 ## NOTE: Enable execution on installation helper scripts  
 chmod +x ./install-scripts/*.sh
 
+./install-scripts/ensure-common-directories.sh
 ./install-scripts/stow-config.sh
+./install-scripts/install-packages.sh
