a3a02aa9cbd461f34a0e714991890c23b01097e1
jdubba <148065+jdubba@users.noreply.github.com>
2025-05-10 23:16:07 -0500

    Fixed permissions issue with link script.

--- install-scripts/link-scripts.sh
+++ install-scripts/link-scripts.sh
@@ -3,7 +3,5 @@
 # NOTE:  Symlink all items in ./scripts to ~/.local/bin
 
 for file in ./scripts; do                                                                                                                                 
-    sudo ln -s "$file" ~/.local/bin/$(basename "$file")                                                                                                                    
+    ln -s "$file" ~/.local/bin/$(basename "$file")                                                                                                                    
 done
-
-
