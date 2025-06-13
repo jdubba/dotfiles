01400aafafe44bb16ad78f6dc72349888cfb0667
Justin Williams <148065+jdubba@users.noreply.github.com>
2025-06-13 13:04:39 -0500

    Removed all software installation and setup functionality.  Simplified to basic stow config file management.

--- install.sh
+++ install.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
 
-## NOTE: Enable execution on installation helper scripts  
-chmod +x ./install-scripts/*.sh
+stow_directory=$(pwd)
+stow_target_directory=$HOME
 
-./install-scripts/ensure-common-directories.sh
-
-./install-scripts/add-repos.sh
-
-if command -v apt-get >/dev/null 2>&1; then
-    sudo apt update
-fi
-
-./install-scripts/install-aur-helper.sh
-./install-scripts/install-packages.sh
-./install-scripts/install-ble.sh.sh
-./install-scripts/install-starship.sh
-./install-scripts/install-yai.sh
-./install-scripts/install-neovim.sh
-./install-scripts/install-kitty.sh
-./install-scripts/install-webp.sh
-./install-scripts/install-xh.sh
-./install-scripts/install-fonts.sh
-
-./install-scripts/stow-config.sh
-./install-scripts/link-scripts.sh
-
-./install-scripts/install-gnome-extras.sh
+## NOTE:  Establish config symlinks, using --adopt followed by git restore to ensure links for any files that already exist
+stow -v --adopt -d $stow_directory -t $stow_target_directory config
+git restore .
